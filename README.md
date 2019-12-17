29c52b475f401232ca1b507825e0710b859002b6
mathiascode <mail@mathias.is>
2019-12-17 18:00:31 +0200

    Enforce checkstyle

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# commandspy
+# CommandSpy
 
-Source code for the CommandSpy plugin on the Kaboom server
+CommandSpy is a Bukkit plugin that allows you to spy on players' commands.
+
+The plugin is created for the Kaboom server.
+
+## Commands
+
+| Command | Alias | Permission | Description |
+| ------- | ----- | ---------- | ----------- |
+|/commandspy | /c, /cs, /cspy | commandspy.command | Allows you to spy on players' commands|
+
+## Compiling
+
+Use [Maven](https://maven.apache.org/) to compile the plugin.
+```bash
+mvn package
+```
+The generated .jar file will be located in the target/ folder.
+
+## License
+[Unlicense](https://unlicense.org/)
--- src/main/java/pw/kaboom/commandspy/Main.java
+++ src/main/java/pw/kaboom/commandspy/Main.java
@@ -2,29 +2,46 @@ package pw.kaboom.commandspy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-
 import org.bukkit.entity.Player;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin implements CommandExecutor, Listener {
+	@Override
 	public void onEnable() {
-		this.getCommand("commandspy").setExecutor(new CommandCommandSpy());
+		this.getCommand("commandspy").setExecutor(this);
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 
+	public boolean commandSpyCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
+		if (sender instanceof ConsoleCommandSender) {
+			sender.sendMessage("Command has to be run by a player");
+		} else {
+			final Player player = (Player) sender;
+			final JavaPlugin plugin = JavaPlugin.getPlugin(Main.class);
+
+			if (plugin.getConfig().contains(player.getUniqueId().toString())) {
+				plugin.getConfig().set(player.getUniqueId().toString(), null);
+				plugin.saveConfig();
+				player.sendMessage("Successfully disabled CommandSpy");
+			} else {
+				plugin.getConfig().set(player.getUniqueId().toString(), true);
+				plugin.saveConfig();
+				player.sendMessage("Successfully enabled CommandSpy");
+			}
+		}
+		return true;
+	}
+
 	@EventHandler
-	void onCommand(PlayerCommandPreprocessEvent event) {
+	void onCommand(final PlayerCommandPreprocessEvent event) {
 		final Player commandRunner = event.getPlayer();
 
 		for (Player messageTarget: Bukkit.getOnlinePlayers()) {
@@ -43,7 +60,7 @@ public class Main extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler
-	void onSignChange(SignChangeEvent event) {
+	void onSignChange(final SignChangeEvent event) {
 		final Player signPlacer = event.getPlayer();
 
 		for (Player messageTarget: Bukkit.getOnlinePlayers()) {
@@ -62,27 +79,5 @@ public class Main extends JavaPlugin implements Listener {
 				}
 			}
 		}
-	}
-}
-
-class CommandCommandSpy implements CommandExecutor {
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender instanceof ConsoleCommandSender) {
-			sender.sendMessage("Command has to be run by a player");
-		} else {
-			final Player player = (Player) sender;
-			final JavaPlugin plugin = JavaPlugin.getPlugin(Main.class);
-	
-			if (plugin.getConfig().contains(player.getUniqueId().toString())) {
-				plugin.getConfig().set(player.getUniqueId().toString(), null);
-				plugin.saveConfig();
-				player.sendMessage("Successfully disabled CommandSpy");
-			} else {
-				plugin.getConfig().set(player.getUniqueId().toString(), true);
-				plugin.saveConfig();
-				player.sendMessage("Successfully enabled CommandSpy");
-			}
-		}
-		return true;
 	}
 }
